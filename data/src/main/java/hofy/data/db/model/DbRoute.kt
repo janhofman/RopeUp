0992f1fc2e6f8973791873b52b8ed4f5bb0b16c9
Jan Hofman <jan.hofman@eman.cz>
2021-11-04 16:10:45 +0100

    todo

--- data/src/main/java/hofy/data/db/model/DbRoute.kt
+++ data/src/main/java/hofy/data/db/model/DbRoute.kt
@@ -4,6 +4,8 @@ import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 
+
+//TODO add firebase ID and restrict duplicity
 @Entity
 data class DbRoute(
     @PrimaryKey(autoGenerate = true) val uid: Int = 0,
